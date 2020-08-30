c6b6148136382e1783857735b693fa6783af26b5
Tyler Nguyen <nguyentyler341@gmail.com>
2020-08-30 19:16:10 -0400

    Changes db connect to connect to localhost instead of MongoAtlas

--- server.js
+++ server.js
@@ -5,39 +5,21 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 4000;
 
-const uri = "mongodb+srv://tylernguyen:tTt03181995@cluster0.gud8x.mongodb.net/guidebook?retryWrites=true&w=majority";
-
 app.use(cors());
 app.use(bodyParser.json());
 
-//Fix depcrecation warnings
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect(uri, { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/guidebook', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function () {
-    console.log("MongoDB database connection established successfully!!!");
+    console.log("MongoDB database connection established successfully");
 })
-
 
 app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
-});
-
-//Routing logic
-const guidebookRoutes = express.Router();
-app.use('/guidebook', guidebookRoutes);
-
-guidebookRoutes.route('/').get(function (req, res) {
-    guidebook.find(function (err, todos) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(todos);
-        }
-    });
 });
